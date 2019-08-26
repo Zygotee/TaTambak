5ee2168f8938c5c7aa2a7c024b6d799c44a12372
Zygotee <bimasetiawan1@gmail.com>
2019-08-26 18:36:50 +0700

    add photo

--- resources/views/layouts/admin/dashboard-admin.blade.php
+++ resources/views/layouts/admin/dashboard-admin.blade.php
@@ -6,8 +6,8 @@
         <div class="col-md-12">
             <div class="card full-height">
                 <div class="card-body">
-                    <div class="card-title">Statistik Realtime</div>
-                    <div class="card-category">Informasi Data Realtime Sensor</div>
+                    <div class="card-title">Data Realtime</div>
+                    <div class="card-category">Informasi Data Realtime Sensor Terbaru</div>
                     <div class="d-flex flex-wrap justify-content-around pb-2 pt-4">
                         <div class="px-2 pb-2 pb-md-0 text-center">
                             <div id="circles-1"></div>
